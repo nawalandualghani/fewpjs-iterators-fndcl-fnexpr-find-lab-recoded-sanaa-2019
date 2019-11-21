68742986df450268310e92a884e9696609db575b
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-21 09:12:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const testVar = {}
+const testVar = {
+
+}
 
 function testFunc() {
   return "hi"
+}
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
 }
